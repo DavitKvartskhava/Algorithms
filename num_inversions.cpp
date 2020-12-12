97f461e3024896a77a6dcaaf80d3e2d71535d69a
Davit Kvarskhava <dkvart17@earlham.edu>
2020-12-12 16:31:22 -0600

    Implemented divide and conquer approach for counting number of inversions

--- num_inversions.cpp
+++ num_inversions.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 /*Different methods for counting number of inversions in an array
   Inversion: a pair of indices (i, j) where i < j but arr[i] > arr[j]
+  Input: array containing integers 1,2,3... in some arbitrary order
+  Output: # of inversions
 */
 
 
@@ -18,13 +20,59 @@ int num_inversions_brute_force(int arr[], int length){
 	}
 	return counter;
 }
+
+/*
+	Divide and conquer approach 
+	Complexity: O(nlogn)
+	Idea: piggyback on mergesort
+*/
+int num_split_inv_merge_and_count(int arr[], int low, int high){
+	int mid = (low+high)/2;
+	int i = low, j = mid+1; //i pointer for left half, j pointer for right half. For all i, j -> i < j
+	int k = 0; //index for temp arr
+	int temp[high-low+1];
+
+	int num_split_inv = 0;
+
+	while(i <= mid && j <= high){
+		if(arr[i] < arr[j]){
+			temp[k++] = arr[i++]; 
+		} else {
+			//when we copy from right half, we know for sure that 
+			//the number of the remaining elements in the left array
+			//counts as a split inversion.
+			num_split_inv += mid-i+1;
+			temp[k++] = arr[j++];
+		}
+	}
+
+	while(i <= mid){ temp[k++] = arr[i++]; }
+	while(j <= high){temp[k++] = arr[j++]; }
+
+	return num_split_inv;
+
+}
+int num_inversions_sort_and_count(int arr[], int low, int high){
+	if (low == high) {//in length of the array = 1
+		return 0;
+	} else {
+		int mid = (low+high)/2, total = 0;
+		total += num_inversions_sort_and_count(arr, low, mid);//sort and count left half
+		total += num_inversions_sort_and_count(arr, mid+1, high);//sort and count right half
+		total += num_split_inv_merge_and_count(arr, low, high);
+
+		return total;
+	}
+}
+
 int main(){
 	const int length = 6;
-	// int arr[length] = {1,3,5,2,4,6};
-	int arr[length] = {6,5,4,3,2,1};
+	int arr[length] = {1,3,5,2,4,6};
+	// int arr[length] = {6,5,4,3,2,1};
 
-	
-	int result = num_inversions_brute_force(arr, length);
+
+	// int result = num_inversions_brute_force(arr, length);
+	int result = num_inversions_sort_and_count(arr, 0, length-1);
 	cout<<"Number of inversions is: "<<result<<endl;
  	return 0;
 }
